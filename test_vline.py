5d3eb09a97ab197da3bb4cdf72d2515bb758fd0d
Benoit FREMON <bfremon@gmail.com>
2020-11-12 21:25:33 +0100

    tests files added

---
+++ test_vline.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd(), 'pl0t'))
+from pl0t import *
+
+x1 = np.random.normal(10, 1, 10**5)
+
+hist(x1)
+vline(15, color='y')
+shw()
+cls()
+
+hist(x1)
+vline(15, color='r', ls='dashed')
+shw()
