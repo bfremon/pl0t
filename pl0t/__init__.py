6e798ce6a3a91eb24fa77ef2e060ee6288a3eae6
Benoit FREMON <bfremon@gmail.com>
2020-11-14 21:41:57 +0100

    hline(), bplt(), scatter(): new funcs

--- pl0t/__init__.py
+++ pl0t/__init__.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 palette = 'deep'
 
-def hist(*datas, labels=None, stat='count', palette=palette):
+def hist(*datas, labels=None, stat='count', palette=palette, **args):
     d = {}
     i = 0
     for v in datas:
@@ -21,17 +21,31 @@ def hist(*datas, labels=None, stat='count', palette=palette):
     sns.histplot(data=long_df, x='value', hue='variable',
                   stat=stat, palette=palette)
 
+def bplt(*datas):
+    sns.boxplot(*datas)
     
 def vline(x, color='r', **args):
-    '''Plot a vertical line
-    color: matplotlib color of vertical line
+    '''Plot a vertical line going through x
+    color: matplotlib color of line
     *args: other axvline() params
     '''
-    
     if color:
         plt.axvline(x, color=color, **args)
     else:
         plt.axvline(x, **args)
+
+def hline(y, color='b', **args):
+    '''Plot an horizontal line going through y
+    color: matplotlib color of line
+    *args: other axvline() params
+    '''
+    if color:
+        plt.axhline(y, color=color, **args)
+    else:
+        plt.axhline(y, **args)
+
+def scatter(*datas):
+    sns.scatterplot(*datas, **args)
 
         
 def shw():
