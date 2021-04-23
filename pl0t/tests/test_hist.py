bff02882ed81f42d05cc3fccfc3ac3a1c4591eee
Benoit FREMON <bfremon@gmail.com>
2021-04-23 21:47:27 +0200

    sample size reduced

--- pl0t/tests/test_hist.py
+++ pl0t/tests/test_hist.py
@@ -7,10 +7,10 @@ from pl0t import *
 from . import tutils as tu
 f_prefix = 'test_hist-'
 
-x1 = np.random.normal(10, 1, 10**5)
-x2 = np.random.normal(12, 2, 10**5)
-x3 = np.random.normal(14, 4, 10**5)
-x4 = np.random.normal(16, 6, 10**5)
+x1 = np.random.normal(10, 1, 10**2)
+x2 = np.random.normal(12, 2, 10**2)
+x3 = np.random.normal(14, 4, 10**2)
+x4 = np.random.normal(16, 6, 10**2)
 #    hist(x1, x2, x3, x4)
 hist(x1, x2, x3, x4, stat='probability')
 title('Test title')
