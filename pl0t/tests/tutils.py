6107ac2c9d88ebd8f959bd1d38ba6e69e27fea96
Benoit FREMON <bfremon@gmail.com>
2021-04-23 21:49:15 +0200

    rnd_str(): public func, return string changed to 8

--- pl0t/tests/tutils.py
+++ pl0t/tests/tutils.py
@@ -4,11 +4,11 @@ import pl0t
 import string
 import random
 
-def _rnd_str(l=15):
+def rnd_str(l=8):
     ret = ''
     for i in range(l):
         ret += random.choice(string.ascii_letters)
     return ret
 
 def save(pfx):
-    pl0t.save(fname = pfx + _rnd_str())
+    pl0t.save(fname = pfx + rnd_str())
