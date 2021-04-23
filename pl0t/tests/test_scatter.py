5d33552d6d6def9f467ccd65975b7ddab6b31376
Benoit FREMON <bfremon@gmail.com>
2021-04-23 21:47:50 +0200

    new file

---
+++ pl0t/tests/test_scatter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import os
+import sys
+from pl0t import *
+from . import tutils as tu
+
+f_prefix = 'test_scatter'
+
+x1 = np.random.normal(10, 1, 10**5)
+x2 = np.random.normal(12, 2, 10**5)
+x3 = np.random.normal(14, 4, 10**5)
+x4 = np.random.normal(16, 6, 10**5)
+#    hist(x1, x2, x3, x4)
+title('Scatter plot title')
+scat(x=x1, y=x2)
+tu.save(f_prefix)
+cls()
