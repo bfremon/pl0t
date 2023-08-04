a4a6b6628a903ccb8dfc73fc442cdd3321dbc5f7
Benoit FREMON <ben@in.volution.fr>
2023-08-04 18:25:43 +0200

    added test with hue parameter

--- pl0t/tests/test_bplt.py
+++ pl0t/tests/test_bplt.py
@@ -19,13 +19,19 @@ vals = [ scipy.stats.norm.rvs(mu * np.random.random(),
                               std * np.random.random(),
                               vals_n) for i in range(plots_n) ]
 
-dat = {'type': [], 'cat': [], 'val': []}
+dat = {'type': [], 'cat': [], 'val': [], 'hue_cat': []}
 
 for i in range(plots_n):
     cat = tu.rnd_str(8)
     dat['type'].extend([ j for j in range(vals_n) ])
     dat['cat'].extend([ cat for j in range(vals_n) ])
     dat['val'].extend(vals[i])
+    
+hue_cat_labs = ['A', 'B', 'C', 'D', 'E']
+hue_cat_len = int(plots_n * vals_n / len(hue_cat_labs))
+for c in hue_cat_labs:
+    dat['hue_cat'].extend([ c for i in range(hue_cat_len) ])
+
 df = pd.DataFrame(dat)
 
 title('Boxplot title')
@@ -38,3 +44,9 @@ r = bplt('cat', 'val', df)
 r.set(xscale='log')
 tu.save(f_prefix + 'ret_x_scale_log')
 clr()
+
+r = bplt('cat', 'val', df, hue = 'hue_cat')
+r.set(xscale='log')
+tu.save(f_prefix + 'ret_x_scale_log')
+clr()
+
