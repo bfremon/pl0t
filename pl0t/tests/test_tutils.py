d08c8ac7112f6cba11598b2d13d2f31b7d2fe866
Benoit FREMON <ben@in.volution.fr>
2025-02-11 21:58:26 +0100

    gen_datum_labels() unit tested

--- pl0t/tests/test_tutils.py
+++ pl0t/tests/test_tutils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 from __future__ import annotations
+import os
 from pl0t import *
 from . import tutils
+import pandas as pd
 import unittest
 import matplotlib
 matplotlib.use('Agg')
@@ -22,14 +24,44 @@ class test_tutils(unittest.TestCase):
             plt_data = [ i for i in range(10) ]
             scat(x = plt_data, y = plt_data)
             ref_im_id = 'tutils_eval_im'
-            tutils.save(ref_im_id, dest_path = tutils.ref_im_path,
+            tutils.save(ref_im_id, dest_path = tutils.datum_path,
                  rand_str_prefix = False)
-            self.assertTrue(tutils.eval_im(fig, ref_im_id))
+            self.assertTrue(tutils.eval_im(fig, ref_im_id, profile = False))
             plt.close()
 
+            
             fig, ax = plt.subplots()
             plt_data = [ 0 for i in range(10) ]
             scat(x = plt_data, y = plt_data)
             ref_im_id = 'tutils_eval_im'
-            self.assertFalse(tutils.eval_im(fig, ref_im_id))
+            self.assertFalse(tutils.eval_im(fig, ref_im_id, profile = False))
+
+            ref_im_id = 'tutils_eval_im_create'
+            self.assertTrue(tutils.eval_im(fig, ref_im_id, profile = False))
             plt.close()
+                
+
+        def test_gen_datum_labels(self):
+            out_path = os.path.join(tutils.tmp_path,
+                                                'test_gen_rand_labels.txt')
+            n_labels = 10
+            tutils.gen_datum_labels(out_path, n_labels)
+            line_cnt = 0
+            with open(out_path, 'r') as f_r:
+                for l in f_r.readlines():
+                    self.assertTrue(len(l) == 9)
+                    line_cnt += 1
+            self.assertTrue(line_cnt == n_labels)
+
+
+        def test_gen_datum_data(self):
+            out_path = os.path.join(tutils.tmp_path,
+                                    'test_datum_data.csv')
+            tutils.gen_datum_data(out_path)
+            out_data = pd.read_csv(out_path, sep = ';', index_col = [0])
+            self.assertTrue(list(out_data.columns) == ['type', 'cat', 'val', 'hue_cat'])
+            
+                              
+        def tearDown(self):
+            for f in os.listdir(tutils.tmp_path):
+                os.unlink(os.path.join(tutils.tmp_path, f))
