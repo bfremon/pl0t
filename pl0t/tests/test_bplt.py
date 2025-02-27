71b62d9fd99cbfa2323cacc1e6533d790eb42351
Benoit FREMON <ben@in.volution.fr>
2025-02-27 21:45:56 +0100

    listoflists unit testing added

--- pl0t/tests/test_bplt.py
+++ pl0t/tests/test_bplt.py
@@ -12,13 +12,15 @@ class test_bplot(unittest.TestCase):
     def setUp(self) -> None:
         self.df_data = tu.read_datum_data(kind = 'df')
         self.ser_data = tu.read_datum_data(kind = 'series')
-
+        self.listoflists_data = tu.read_datum_data(kind = 'listoflists')
+        self.labels = tu.read_datum_data(kind = 'labels')
+        
         
     def test_bplt_univariate(self) -> None:
         plt_data = self.df_data['val'].iloc[:10]
         im_id = 'bplt_univariate_list'
         fig, ax = plt.subplots()
-        title('Univariate list')
+        title('Univariate list, default label')
         bplt(list(plt_data))
         self.assertTrue(tu.eval_im(fig, im_id))
         plt.close()
@@ -28,7 +30,7 @@ class test_bplot(unittest.TestCase):
         plt_data = self.df_data['val'].iloc[:10]
         im_id = 'bplt_univariate_list_label'
         fig, ax = plt.subplots()
-        title('Univariate list with label')
+        title('Univariate list with label lab')
         bplt(list(plt_data), cat = 'lab')
         self.assertTrue(tu.eval_im(fig, im_id))
         plt.close()
@@ -38,7 +40,7 @@ class test_bplot(unittest.TestCase):
         plt_data = self.ser_data
         im_id = 'bplt_univariate_series'
         fig, ax = plt.subplots()
-        title('Univariate series')
+        title('Univariate series - Series, default label')
         bplt(plt_data)
         self.assertTrue(tu.eval_im(fig, im_id))
         plt.close()
@@ -48,11 +50,40 @@ class test_bplot(unittest.TestCase):
         plt_data = self.ser_data
         im_id = 'bplt_univariate_series_label'
         fig, ax = plt.subplots()
-        title('Univariate series')
+        title('Univariate series - Series with label prat')
         bplt(plt_data, cat = 'prat')
         self.assertTrue(tu.eval_im(fig, im_id))
         plt.close()
-    
+
+        
+    def test_bplt_univariate_listoflists(self) -> None:
+        plt_data = self.listoflists_data
+        im_id = 'bplt_univariate_listofflist'
+        fig, ax = plt.subplots()
+        title('Univariate series - List of lists with default labels ?')
+        bplt(plt_data)
+        self.assertTrue(tu.eval_im(fig, im_id))
+        plt.close()
+
+
+    def test_bplt_univariate_listoflists_label(self) -> None:
+        plt_data = self.listoflists_data
+        im_id = 'bplt_univariate_listofflist_label'
+        fig, ax = plt.subplots()
+        title('Univariate series - List of lists with label lab')
+        self.assertRaises(SyntaxError, bplt, plt_data, cat = 'listoflists lab')
+#        self.assertTrue(tu.eval_im(fig, im_id))
+        plt.close()
+
+        plt_data = self.listoflists_data
+        im_id = 'bplt_univariate_listofflist_label'
+        labs = [ self.labels[i] for i in range(len(plt_data)) ]
+        fig, ax = plt.subplots()
+        title('Univariate series - List of lists with true labels')
+        bplt(plt_data, cat = labs)
+        self.assertTrue(tu.eval_im(fig, im_id))
+        plt.close()
+ 
         
     def test_bplt_std(self) -> None: 
         im_id = 'bplt_std'
